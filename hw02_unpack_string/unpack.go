22602ecf0b96df5586a4fd5aa81d9a05175089d0
Михаил Овечкин <movechkin@diasoft.ru>
2024-11-08 02:26:50 +0300

    HW1 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,44 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	var buffer *rune
+	asterisk := false
+	output := strings.Builder{}
+
+	for _, char := range input {
+		if digit, err := strconv.Atoi(string(char)); err == nil && !asterisk {
+			if buffer == nil {
+				return "", ErrInvalidString
+			}
+
+			output.WriteString(strings.Repeat(string(*buffer), digit))
+			buffer = nil
+			continue
+		} else if buffer != nil {
+			output.WriteRune(*buffer)
+			buffer = nil
+		}
+
+		switch {
+		case asterisk:
+			buffer = &char
+			asterisk = false
+		case char == '\\':
+			asterisk = true
+		default:
+			buffer = &char
+		}
+	}
+
+	if buffer != nil {
+		output.WriteRune(*buffer)
+	}
+	return output.String(), nil
 }
