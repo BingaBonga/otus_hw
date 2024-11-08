7bf5d86d2168e346b9272fc4452381ac1b8a93e0
Михаил Овечкин <movechkin@diasoft.ru>
2024-11-08 23:29:50 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,36 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"math"
+	"sort"
+	"strings"
+)
+
+func Top10(input string) []string {
+	splitText := strings.Fields(input)
+	wordCountMap := make(map[string]int)
+
+	for _, word := range splitText {
+		if word == "" {
+			continue
+		}
+
+		wordCountMap[word]++
+	}
+
+	worlds := make([]string, 0, len(wordCountMap))
+	for word := range wordCountMap {
+		worlds = append(worlds, word)
+	}
+
+	sort.SliceStable(worlds, func(i, j int) bool {
+		if wordCountMap[worlds[i]] == wordCountMap[worlds[j]] {
+			return worlds[i] < worlds[j]
+		}
+
+		return wordCountMap[worlds[i]] > wordCountMap[worlds[j]]
+	})
+
+	returnCount := int(math.Min(float64(len(worlds)), float64(10)))
+	return worlds[:returnCount]
 }
