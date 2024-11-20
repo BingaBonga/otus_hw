8b1b54ce36e88e72fa56b1ebba2f0c45f0a95db9
Михаил Овечкин <movechkin@diasoft.ru>
2024-11-20 09:30:50 +0300

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,53 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := make(chan interface{})
+
+	go func() {
+		bufferChannel := make(chan interface{}, len(stages))
+		go readChannelWithDone(in, bufferChannel, done)
+
+		for _, stage := range stages {
+			outChannel := stage(bufferChannel)
+
+			bufferChannel = make(chan interface{}, len(stages))
+			go readChannel(outChannel, bufferChannel)
+		}
+
+		go readChannelWithDone(bufferChannel, out, done)
+	}()
+
+	return out
+}
+
+func readChannelWithDone(from <-chan interface{}, to chan<- interface{}, done <-chan interface{}) {
+	defer close(to)
+
+	for {
+		select {
+		case <-done:
+			return
+		case v := <-from:
+			if v == nil {
+				return
+			}
+
+			to <- v
+		}
+	}
+}
+
+func readChannel(from <-chan interface{}, to chan<- interface{}) {
+	defer close(to)
+
+	for {
+		select {
+		case v := <-from:
+			if v == nil {
+				return
+			}
+
+			to <- v
+		}
+	}
 }
