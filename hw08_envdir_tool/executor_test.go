8020fc328dddea6a46ff8d1030680e775bca3a98
Михаил Овечкин <movechkin@diasoft.ru>
2025-01-03 06:30:57 +0300

    HW8 is completed

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,19 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("test cmd", func(t *testing.T) {
+		env := map[string]EnvValue{
+			"FOO": {"foo", false},
+			"BAR": {"bar", false},
+		}
+		cmd := []string{"/bin/bash", "echo -e \"${FOO}${BAR}\""}
+		code := RunCmd(cmd, env)
+
+		assert.Equal(t, 0, code)
+	})
 }
