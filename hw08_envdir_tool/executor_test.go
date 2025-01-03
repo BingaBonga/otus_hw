e78fe0ac48e05d78ffd09f5b0e550c41e7b3381e
Михаил Овечкин <movechkin@diasoft.ru>
2025-01-03 07:02:06 +0300

    HW8 is completed

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 
 	//nolint:depguard
@@ -17,5 +18,17 @@ func TestRunCmd(t *testing.T) {
 		code := RunCmd(cmd, env)
 
 		require.Equal(t, 0, code)
+	})
+
+	t.Run("test cmd with unbound variable", func(t *testing.T) {
+		os.Setenv("FOO", "foo")
+		env := map[string]EnvValue{
+			"FOO": {"foo", true},
+			"BAR": {"bar", false},
+		}
+		cmd := []string{"/bin/bash", "-c", "echo $FOO$BAR"}
+		code := RunCmd(cmd, env)
+
+		require.Equal(t, 127, code)
 	})
 }
